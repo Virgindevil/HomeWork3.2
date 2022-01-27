a47f3a728edcbd0381dc06e478fb2e70e18f40a0
S.Matuzov <98310589+Virgindevil@users.noreply.github.com>
2022-01-27 14:57:15 +0400

    Add files via upload

--- Homework3.2/Program.cs
+++ Homework3.2/Program.cs
@@ -10,81 +10,70 @@ namespace HomeWork3._2
     {
         static void Main(string[] args)
         {
-            int sum = 0;            
-            Random random = new Random();
-            Random rand = new Random();
-            Console.ForegroundColor = ConsoleColor.DarkGreen;
-            Console.WriteLine("Добро пожаловать в блекджек на минималках!", 30);
+            int sum = 0;  
+            Console.ForegroundColor = ConsoleColor.DarkGreen;            
             Console.WriteLine("Сколько у вас карт на руке?");
             int cards = int.Parse(Console.ReadLine());     
             for (int i = 0; i < cards; i++)
             {
-                Console.WriteLine("Какая у вас карта? Если с числом, нажмите 1. Если с картинкой - нажмите 2.");
-                int swap = int.Parse(Console.ReadLine());
-                if (swap == 1)
+                Console.WriteLine("Какая у вас карта? Подсказка: Карты от 2 до 10 и Валет - J, Дама -  Q, Король -  K, Туз - T.");
+                switch (Console.ReadLine())
                 {
-                    Console.WriteLine("Введите номинал карты: (Подсказка: От 2 до 10)");
-                    int number = int.Parse(Console.ReadLine());
-                    if (number <= 1)
-                    {
-                        Console.WriteLine("Не жульничайте! Введите число от 2 до 10!");
-                        Console.WriteLine();
-                        int rightnumber = int.Parse(Console.ReadLine());
-                        sum = sum + rightnumber;
-                    }
-                    else
-                    {
-                        sum = sum + number;
-                    }
-                }
-                else
-                {
-                    Console.WriteLine("Введите букву карты: (Подсказка: J - Валет, Q - Дама, K - Король,  T - Туз.)");
-                    char picture = char.Parse(Console.ReadLine());
-                    switch (picture)
-                    {
-                        case 'J':
-                            sum = (sum + 10);
-                            break;
-                        case 'j':
-                            sum = (sum + 10);
-                            break;
-                        case 'Q':
-                            sum = (sum + 10);
-                            break;
-                        case 'q':
-                            sum = (sum + 10);
-                            break;
-                        case 'K':
-                            sum = (sum + 10);
-                            break;
-                        case 'k':
-                            sum = (sum + 10);
-                            break;
-                        case 'T':
-                            sum = (sum + 10);
-                            break;
-                        case 't':
-                            sum = (sum + 10);
-                            break;
-                    }                    
+                    case "2":
+                        sum = (sum + 2);
+                        break;
+                    case "3":
+                        sum = (sum + 3);
+                        break;
+                    case "4":
+                        sum = (sum + 4);
+                        break;
+                    case "5":
+                        sum = (sum + 5);
+                        break;
+                    case "6":
+                        sum = (sum + 6);
+                        break;
+                    case "7":
+                        sum = (sum + 7);
+                        break;
+                    case "8":
+                        sum = (sum + 8);
+                        break;
+                    case "9":
+                        sum = (sum + 9);
+                        break;
+                    case "10":
+                        sum = (sum + 10);
+                        break;
+                    case "J":
+                        sum = (sum + 10);
+                        break;
+                    case "j":
+                        sum = (sum + 10);
+                        break;
+                    case "Q":
+                        sum = (sum + 10);
+                        break;
+                    case "q":
+                        sum = (sum + 10);
+                        break;
+                    case "K":
+                        sum = (sum + 10);
+                        break;
+                    case "k":
+                        sum = (sum + 10);
+                        break;
+                    case "t":
+                        sum = (sum + 10);
+                        break;
+                    case "T":
+                        sum = (sum + 10);
+                        break;                    
                 }
             }
             Console.WriteLine();
             Console.WriteLine(sum);
-            if (sum == 21)
-            {
-                Console.WriteLine("Отлично! Вы победили!");
-            }
-            if (sum < 21)
-            {
-                Console.WriteLine("Увы! Недобор!");
-            }
-            if (sum > 21)
-            {
-                Console.WriteLine("Увы! Перебор!");
-            }
-
             Console.ReadKey();
         }
     }
